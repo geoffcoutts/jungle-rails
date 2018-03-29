ea773e3c2502c30454ad6f09cf9a10b0267de351
Geoff Coutts <geoff.coutts@gmail.com>
2018-03-29 20:31:18 +0000

    added user login and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,15 @@ Rails.application.routes.draw do
     delete :remove_item
   end
 
+  resource :session,
+    only: [:new, :create, :destroy],
+    path_names: { new: 'login', create: 'login' }
+
+  resources :users,
+    only: [:new, :create],
+    path_names: { new: 'register' }
+
+
   resources :orders, only: [:create, :show]
 
   namespace :admin do
