dbaec7f1fda55969a3507021d89dba2429c976e6
Geoff Coutts <geoff.coutts@gmail.com>
2018-03-30 15:34:20 +0000

    Messages successfully print in console, created plain text version as well.

--- app/mailers/order_receipt_mailer.rb
+++ app/mailers/order_receipt_mailer.rb
@@ -6,7 +6,7 @@ class OrderReceiptMailer < ApplicationMailer
     @order = order
     mail(to: @order.email, subject: 'Thanks for Ordering From Jungle') do |format|
       format.html { render locals: { order: @order } }
-      # format.text { render locals: { order: order } }
+      format.text { render locals: { order: order } }
     end
   end
 
