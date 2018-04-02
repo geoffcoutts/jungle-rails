c6ba9278296fe5127a073fb3022a30c31be0d0ca
Geoff Coutts <geoff.coutts@gmail.com>
2018-04-02 17:07:31 +0000

    Added validation and check for minimum and maximum length of password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates_length_of :password, in: 8..16
   validates_confirmation_of :password
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def authenticate_with_credentials
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,15 @@ RSpec.describe User, type: :model do
   context 'Validations' do
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_confirmation_of(:password) }
+    it { is_expected.to validate_length_of(:password)
+    .is_at_least(8)
+    .is_at_most (16)
+    }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
-    # binding.pry
+    binding.pry
   end
 
 end
