a472b8cb38fc288e0cc01f6319661cde5e1d777d
Geoff Coutts <geoff.coutts@gmail.com>
2018-04-02 16:49:34 +0000

    Added tests for presence of email, first_name, and last_name. Also for uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :password, presence: true
+  validates_confirmation_of :password
   validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe User, type: :model do
 
   context 'Validations' do
     it { is_expected.to validate_presence_of(:password) }
-    it { is_expected.to validate_presence_of(:password_confirmation) }
-
+    it { is_expected.to validate_confirmation_of(:password) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    # binding.pry
   end
 
 end
