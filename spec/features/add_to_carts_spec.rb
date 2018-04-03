a49945c1af59f7edfcfd40ffa5bdb842eab5be5a
Geoff Coutts <geoff.coutts@gmail.com>
2018-04-03 18:04:36 +0000

    Added feature test for adding a single item to cart and it passes

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor adds item to cart", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+      )
+    end
+  end
+
+  scenario "Cart increases by 1" do
+
+    visit root_path
+
+    expect(page).to have_css 'div#navbar', text: 'My Cart (0)'
+
+    page.all('article.product').sample.find('footer a', text: 'Add').click
+
+    expect(page).to have_css 'div#navbar', text: 'My Cart (1)'
+
+    save_screenshot
+  end
+
+end
